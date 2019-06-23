3b059a93214c63d2eca855d3f83a664ed748b8f0
kassim585 <kassim5852@yahoo.co.in>
2019-06-23 11:28:16 +0530

    adding POJOS. Closes #2.

---
+++ src/main/java/guru/springframework/sfgpetclinic/model/Person.java
@@ -0,0 +1,23 @@
+package guru.springframework.sfgpetclinic.model;
+
+public class Person {
+
+    private String firtName;
+    private String lastName;
+
+    public String getFirtName() {
+        return firtName;
+    }
+
+    public void setFirtName(String firtName) {
+        this.firtName = firtName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+}
